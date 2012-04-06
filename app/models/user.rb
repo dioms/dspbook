061929ec671ccc741664f2b220f888da14136afe
Diego Ortiz-Monasterio <dortizmo@asu.edu>
2012-04-06 09:27:26 -0700

    AWS hosting

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,20 @@
 class User < ActiveRecord::Base
-  has_attached_file :profpic, :styles => { :medium => "300x300", :thumb => "100x100" }
-  has_attached_file :resume 
+  has_attached_file :profpic,
+    :styles => { :medium => "300x300", :thumb => "100x100" },
+    :storage => :s3,
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :s3_credentials => {
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  has_attached_file :resume, 
+    :storage => :s3,
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :s3_credentials => {
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
