f4e37f02b185ab2bcd2804303578cefe71d24a4c
Diego Ortiz-Monasterio <dortizmo@asu.edu>
2012-04-06 01:15:03 -0700

    Various changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me,
-    :name, :profpic, :resume
+    :name, :profpic, :resume, :bio
 
   validates_presence_of :name
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120406065824) do
+ActiveRecord::Schema.define(:version => 20120406075657) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(:version => 20120406065824) do
     t.string   "resume_content_type"
     t.integer  "resume_file_size"
     t.datetime "resume_updated_at"
+    t.text     "bio"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
